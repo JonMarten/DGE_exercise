ea790a42716f929629f407d47661a091af9497ff
Jonathan Marten <jonmarten@gmail.com>
2020-07-09 15:28:37 +0100

    Create script to make genome index

---
+++ 1_3_align_reads.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -A PAUL-SL3-CPU
+#SBATCH -p skylake
+#SBATCH --mem 20G
+#SBATCH -n6
+#SBATCH --job-name=genomeindex
+#SBATCH --time=12:0:0
+#SBATCH --output=/rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/dge_test/logs/dge_test_makegenomeindex_%A_%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jm2294@medschl.cam.ac.uk
+
+module load star/2.5.0a
+cd /rds/user/jm2294/rds-jmmh2-projects/interval_rna_seq/dge_test
+
+STAR --runThreadN 6\
+ --runMode genomeGenerate\
+ --genomeDir genome_index\
+ --genomeFastaFiles /home/jm2294/rds/rds-jmmh2-public_databases/GRCh37_reference_fasta/hs37d5.fa\
+ --sjdbGTFfile reference/Homo_sapiens.GRCh37.87.gtf.gz\
+ --sjdbOverhang 149
+ 
+
